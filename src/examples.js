201ac720cbb040ddad2fe9ad2c7efef4f4556d91
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2026-02-05 09:22:14 -0800

    Fixing my name sim and adding gitignore

--- src/examples.js
+++ src/examples.js
@@ -9,7 +9,7 @@ function requireExample(name) {
 }
 
 var examplePairs = [
-  'jeffLoshi',
+  'jeffYoshimi',
   'repeat01',
   'binaryIncrement',
   'divisibleBy3',
@@ -49,5 +49,5 @@ var list = examplePairs.map(function (pair) { return pair[1]; });
 exports.hasID = isExampleID;
 exports.get = get;
 exports.list = list;
-exports.firsttimeDocID = 'jeffLoshi';
+exports.firsttimeDocID = 'jeffYoshimi';
 exports.blankTemplate = requireExample('_template');
